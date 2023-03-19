76877cc39e0bb49432e59e7df95b873ccf0d88d5
NBARK20 <kazdabinbark@gmail.com>
2023-03-19 08:03:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to thz standard erreur
+ * Return 1 (success).
+ */
+int main(void)
+{
+ fput("and that piece of art is useful\") - and that piece of art is useful\r" , stdout);
+return (1)
+}
