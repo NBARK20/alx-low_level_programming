72f7bc7d47dbeaa01f449a8e53f37d2588235993
NBARK20 <kazdabinbark@gmail.com>
2023-03-19 06:49:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printf "Programming is like building a multilingual
+ *puzzle, followed by a new line."
+ * Return: always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
