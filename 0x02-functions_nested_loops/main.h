120f326607056e492828685ceb6adfd8cc4da72e
NBARK20 <kazdabinbark@gmail.com>
2023-03-23 16:22:53 -0700

    1-main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#ifndef main_h
 #define main_h
 int _putchar(char);
 void print_alphabet(void);
@@ -12,3 +11,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+#endif
+
