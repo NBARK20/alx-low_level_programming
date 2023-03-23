d339627ce3ed8a02bb6fd0a0de4104dcbf703077
NBARK20 <kazdabinbark@gmail.com>
2023-03-23 16:28:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#define main_h
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,5 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif
-
