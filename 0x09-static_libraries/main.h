a123cb9005743f0bd08b7f196836cb2016809d80
NBARK20 <kazdabinbark@gmail.com>
2023-04-09 08:24:20 -0700

    libmy

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,7 @@
-#ifndef main_h
-#define  main_h
-#include <stdio.h>
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+		
+int _putchar(char c);		
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
