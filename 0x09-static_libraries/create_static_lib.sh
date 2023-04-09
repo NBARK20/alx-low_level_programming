ccc6d1ed33d7b4425b4bfbeb7b63f7c284fd455b
NBARK20 <kazdabinbark@gmail.com>
2023-04-09 13:43:24 -0700

    ghj

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -c *.c               # compile all .c files into object files
-ar rcs libmy.a *.o      # create static library from object files
-rm *.o                   # remove object files
