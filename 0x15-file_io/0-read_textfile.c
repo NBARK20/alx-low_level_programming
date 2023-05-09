c5c9a222d38983e3105cc923d43d07ad616c7c65
NBARK20 <kazdabinbark@gmail.com>
2023-05-08 19:06:50 -0700

    project

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    int fd, bytes_read;
-    char *buf;
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (filename == NULL) {
-        return 0;
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
-
-    buf = (char *) malloc(sizeof(char) * (letters + 1));
-    if (buf == NULL) {
-        close(fd);
-        return 0;
-    }
-
-    bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1) {
-        free(buf);
-        close(fd);
-        return 0;
-    }
-
-    buf[bytes_read] = '\0';
-
-    if (write(STDOUT_FILENO, buf, bytes_read) != bytes_read) {
-        free(buf);
-        close(fd);
-        return 0;
-    }
-
-    free(buf);
-    close(fd);
-
-    return bytes_read;
+	free(buf);
+	close(fd);
+	return (w);
 }
